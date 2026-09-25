1f37c18e0a4afa65c617e59f445b3408724de668
allisterattard <allister_91@hotmail.com>
2026-09-25 10:31:13 +0200

    feat: azure sql and database scanner plugin

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ ENABLED_PLUGINS = [
     "nsg_plugin",
     "storage_plugin",
     "keyvault_plugin",
-    "redis_plugin"
+    "redis_plugin",
+    "sql_plugin"
 ]
 
 SEVERITY_LEVEL = ["LOW", "MEDIUM", "HIGH", "CRITICAL"]
